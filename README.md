909bad67fa8b02fcb648521f527ee7884298552f
lvpangpang <574818075@qq.com>
2020-09-12 21:22:50 +0800

    项目框架

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # childhood
+
+吕肥肥的小时光
+
+css in js
+动画切换转场
+ts 没有any
+redux
---
+++ client/src/Index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+// import App from '@/router'
+
+function App() {
+  return <div style={{fontSize: '28px',textAlign: 'center'}}>吕肥肥的小时光</div>;
+}
+ReactDOM.render(<App />, document.querySelector('#root'));
